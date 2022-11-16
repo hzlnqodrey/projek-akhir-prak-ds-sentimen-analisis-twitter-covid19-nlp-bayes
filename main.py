d1c8c1a2fa034a375320e9cfd7f8e3bf5867c3ea
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-16 19:13:18 +0700

    case folding

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ import numpy as np
 import tweepy
 import snscrape.modules.twitter as sntwitter
 
+# Get All Covid Sentiment Data from January 1st, 2020 until November 1st, 2022
 query = "covid since:2020-01-01 until:2022-11-01 lang:id"
+limit = 100000 # limit 50k rows
 tweets = []
-limit = 5000
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     if len(tweets) == limit:
         break
     else:
-        tweets.append([tweet.date, tweet.user, tweet.content, tweet.id])
-        
-df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'Tweet ID'])
-print(df)
+        tweets.append([
+            tweet.date,
+            tweet.username,
+            tweet.content
+        ])
 
-# to save to csv
-df.to_csv('tweets_covid.csv')
+filename = 'tweets_covid_dataset_50k_raw_noindex.csv'
+tweets_df = pd.DataFrame(tweets, columns=['Tanggal', 'Username', 'Text'])
+tweets_df.to_csv(filename, index=False)
+print('Scraping has completed!')
